034c1a1396ae142664c39c59a6f2485a5eb685fc
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-02-18 07:19:30 +0600

    Upto Delete Operation, Rental Movie Project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
+import { MovieContext } from "./context";
+
+function App() {
+  const [cartData, setCartData] = useState([]);
+  return (
+    <>
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
+    </>
+  );
+}
+
+export default App;
